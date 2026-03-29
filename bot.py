5da9d46cc188622c8f391d2abf4147b0ceb79cc4
landlos1 <surftrooper@gmail.com>
2026-03-29 16:35:55 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -7,9 +7,8 @@ from config import BOT_TOKEN, COFFEE_SHOP_NAME, COFFEE_MENU, SIZE_OPTIONS, MILK_
 from database import (
     init_db, get_or_create_user, get_user_by_telegram_id, get_on_duty_admin,
     set_admin_on_duty, create_order, get_order, update_order_status,
-    add_bonuses_to_user, use_bonuses, get_all_users,
-    get_user_orders, get_orders_statistics, update_order_comment,
-    get_all_non_completed_orders
+    add_bonuses_to_user, use_bonuses, get_pending_orders, get_all_users,
+    get_user_orders, get_orders_statistics, update_order_comment
 )
 
 # Настройка логирования
